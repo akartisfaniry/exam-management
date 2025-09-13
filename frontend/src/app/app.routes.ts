1f541f0bbcf37f8920cba73cdc6a5eaa58785a75
Sitraka Faniry <sitraka@adrware.mg>
2025-09-13 10:48:20 +0300

    feat: add exam list component mock and routing, integrate

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ExamListComponent} from './components/exam-list/exam-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/exams', pathMatch: 'full' },
+  { path: 'exams', component: ExamListComponent },
+];
---
+++ frontend/src/app/components/exam-list/exam-list.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-exam-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './exam-list.component.html',
+  styleUrl: './exam-list.component.scss'
+})
+export class ExamListComponent{
+}
